5be39b87da86eac4d5333dacd44c8aef6869387a
Bishesh Singh Chauhan <bisheshsinghchauhan@gmail.com>
2026-09-08 12:02:10 +0530

    header,meathodfinder,question

--- exp2.js
+++ exp2.js
@@ -1,4 +1,25 @@
-//perfom CRUD operation on files using fs modules
-
 const fs = require("fs");
-fs.createWriteStream
+fs.readFile("xyz.js", "utf8", (err, data) => {
+    console.log(err,data);
+    console.log("File Content:", data);
+});
+console.log("File Reading finished");
+
+fs.writeFileSync("data.txt" , "hello student" , (err) => {
+    
+    });
+
+fs.appendFile("data.txt", "console.log('Hello World!');", (err) => {
+    if (err) throw err;
+    console.log("Data appended to file!");
+});
+
+fs.writeFile("data.txt", "console.log('Hello World!');", (err) => {
+    if (err) throw err;
+    console.log("File written successfully!");
+});
+
+fs.unlink("data.txt", (err) => {
+    if (err) throw err;
+    console.log("File deleted successfully!");
+});
