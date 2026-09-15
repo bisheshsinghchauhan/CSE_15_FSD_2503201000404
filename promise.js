db1402d98104c2c286d05011120b9e6015433923
Bishesh Singh Chauhan <bisheshsinghchauhan@gmail.com>
2026-09-15 11:28:23 +0530

    promise

---
+++ promise.js
@@ -0,0 +1,20 @@
+const promise = new Promise((resolve , reject) =>
+{
+    // setTimeout(() => {
+    //     resolve("found data");
+
+    // },2000);
+
+    setTimeout(() => {
+        reject("not found data");
+    },2000);
+});
+
+// promise .then((result)=>{
+//     console.log(result);
+    
+
+// });
+promise .catch((error)=>{
+    console.log(error);
+});
